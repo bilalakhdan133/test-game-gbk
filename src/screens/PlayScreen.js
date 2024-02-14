e871ae49fd6067f3a8142400c3e9a8fef3a5a92d
badrol <bilal.akhdan03@gmail.com>
2024-02-14 13:10:20 +0700

    cuma bisa main 1 round

--- src/screens/PlayScreen.js
+++ src/screens/PlayScreen.js
@@ -7,18 +7,18 @@ const choices = ['rock', 'paper', 'scissors'];
 
 
 const PlayScreen = ({route}) => {
+  const { roundTotal } = route.params;
+
   const [playerChoice, setPlayerChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
   const [result, setResult] = useState('');
   const [round, setRound] = useState(1);
   
-  const [playerFlag, setPlayerFlag] = useState(0);
   const [playerWins, setPlayerWins] = useState(0);
-  const [compFlag, setCompFlag] = useState(0);
+  const [compWins, setCompWins] = useState(0);
 
-  const [showModal, setShowModal] = useState(false);
-
-
+  const [allRound, setAllRound] = useState(roundTotal);
+  
   const generateComputerChoice = () => {
     return choices[Math.floor(Math.random() * choices.length)];
   };
@@ -31,52 +31,8 @@ const PlayScreen = ({route}) => {
     setPlayerChoice(playerChoice);
     setComputerChoice(computerChoice);
 
-    // while(winCountPlayer != roundTotal || winCountComp != roundTotal){
-    //   if (playerChoice === computerChoice) {
-    //     setResult('It\'s a tie!');
-    //   } else if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')){
-    //     winCountPlayer++;
-    //   } else {
-    //     winCountComp++;
-    //   }
-    //   setRound(round + 1);
-    // }
+    // ini yang ori
 
-    // for (let i = 0; i < roundTotal; i++) {
-    //   if (playerChoice === computerChoice) {
-    //     setResult('It\'s a tie!');
-    //   } else if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')){
-    //     winCountPlayer++;
-    //   } else {
-    //     winCountComp++;
-    //   }
-    //   setRound(round + 1);
-    // } 
-    // if (winCountPlayer > winCountComp){
-    //   setResult('You win!');
-    //   setPlayerWins(playerWins + 1);
-    // }else{
-    //   setResult('You lose!');
-    // }
-
-    //  for (let i = 0; i < roundTotal; i++) {
-    //   if (i < roundTotal){
-    //     if (playerChoice === computerChoice) {
-    //       setResult('It\'s a tie!');
-    //     } else if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')){
-    //       setResult('You win!');
-    //       setPlayerFlag(playerFlag + 1);
-    //     } else {
-    //       setResult('Comp win!');
-    //       setCompFlag(compFlag+1);
-    //     }
-    //     i++;
-    //     setRound(round+1);
-    //   }else{
-    //     setRound(1);
-    //   }
-    // } 
-  
     if (playerChoice === computerChoice) {
       setResult('It\'s a tie!');
     } else if (
@@ -85,80 +41,26 @@ const PlayScreen = ({route}) => {
       (playerChoice === 'scissors' && computerChoice === 'paper')
     ) {
       setResult('You win!');
-      setPlayerFlag(playerFlag + 1);
+      setPlayerWins(playerWins + 1);
+      // alert('kamu menang!');
     } else {
       setResult('Computer wins!');
-      setCompFlag(compFlag+1);
+      setCompWins(compWins + 1);
+      // alert('kamu kalah :(');
     }
 
-    if (roundTotal == 1){
-      if(
-        (playerChoice === 'rock' && computerChoice === 'scissors') ||
-        (playerChoice === 'paper' && computerChoice === 'rock') ||
-        (playerChoice === 'scissors' && computerChoice === 'paper')
-      ){
-        setPlayerWins(playerWins+1);
-        setCompFlag(0);
-        setPlayerFlag(0);
-        alert('You Win!');
-        setShowModal(true);
-      } else {
-        setCompFlag(0);
-        setPlayerFlag(0);
-        alert('You Lose :(');
-        setShowModal(true);
-      }
-    }
-    
-    if (playerFlag + compFlag < roundTotal) {
+    if (round < roundTotal) {
       setRound(round + 1);
-    }
-    else if(playerFlag == roundTotal-1 || compFlag == roundTotal-1){
+    }else{
       setRound(1);
-      if (playerFlag > compFlag){
-        setPlayerWins(playerWins+1);
-        setCompFlag(0);
-        setPlayerFlag(0);
-        alert('You Win!');
-        setShowModal(true);
-      }else{
-        setCompFlag(0);
-        setPlayerFlag(0);
-        alert('You Lose :(');
-        setShowModal(true);
-      }
-
-
     }
-
-    // ini yang ori
-
-    // if (playerChoice === computerChoice) {
-    //   setResult('It\'s a tie!');
-    // } else if (
-    //   (playerChoice === 'rock' && computerChoice === 'scissors') ||
-    //   (playerChoice === 'paper' && computerChoice === 'rock') ||
-    //   (playerChoice === 'scissors' && computerChoice === 'paper')
-    // ) {
-    //   setResult('You win!');
-    //   setPlayerFlag(playerFlag + 1);
-    // } else {
-    //   setResult('Computer wins!');
-    // }
-
-    // if (round < roundTotal) {
-    //   setRound(round + 1);
-    // }else{
-    //   setRound(1);
-    // }
   
   };
 
   return (
     <div>
       <h1>Rock, Paper, Scissors</h1>
-      <h1>Main berapa round</h1>
-      {/* <h2>Round {round}</h2> */}
+      <h1>Yang sampe {allRound} duluan menang! </h1>
     
       <div>
         <button onClick={() => playRound('rock')}>Rock</button>
@@ -174,8 +76,7 @@ const PlayScreen = ({route}) => {
         )}
         <p>{result}</p>
         <p>Player Wins: {playerWins}</p>
-        <p>Player Flag: {playerFlag}</p>
-        <p>Computer Flag: {compFlag}</p>
+        <p>Computer Wins: {compWins}</p>
       
       </div>
     
---
+++ src/screens/TempPlayScreen.js
@@ -0,0 +1,155 @@
+// ini buat nyimpen yang 1 3 5 round, gadipake
+import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import {Route, IndexRoute} from 'react-router';
+
+const choices = ['rock', 'paper', 'scissors'];
+
+
+const PlayScreen = ({route}) => {
+  const { roundTotal } = route.params;
+
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState('');
+  const [round, setRound] = useState(1);
+  
+  const [playerFlag, setPlayerFlag] = useState(0);
+  const [playerWins, setPlayerWins] = useState(0);
+
+  const [compFlag, setCompFlag] = useState(0);
+  const [compWins, setCompWins] = useState(0);
+
+  const [allRound, setAllRound] = useState(roundTotal);
+  const [showModal, setShowModal] = useState(false);
+  
+  const generateComputerChoice = () => {
+    return choices[Math.floor(Math.random() * choices.length)];
+  };
+
+  const playRound = (playerChoice) => {
+    const computerChoice = generateComputerChoice();
+
+    const { roundTotal } = route.params;
+
+    setPlayerChoice(playerChoice);
+    setComputerChoice(computerChoice);
+
+    if (playerChoice === computerChoice) {
+      setResult('It\'s a tie!');
+    } else if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      setResult('You win!');
+      setPlayerFlag(playerFlag + 1);
+    } else{
+      setResult('Computer wins!');
+      setCompFlag(compFlag+1);
+    }
+
+    if (roundTotal == 1){
+      if(
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+      ){
+        setPlayerWins(playerWins+1);
+        setCompFlag(0);
+        setPlayerFlag(0);
+        // alert('You Win!');
+        setShowModal(true);
+      } else {
+        setCompWins(compWins+1);
+        setCompFlag(0);
+        setPlayerFlag(0);
+        // alert('You Lose :(');
+        setShowModal(true);
+      }
+    }
+    else{
+      if (playerFlag + compFlag < roundTotal ) {
+        setRound(round + 1);
+      }
+      else if(playerFlag <= roundTotal || compFlag <=  roundTotal){
+        setRound(1);
+        if (playerFlag > compFlag){
+          setPlayerWins(playerWins+1);
+          setCompFlag(0);
+          setPlayerFlag(0);
+          // alert('You Win!');
+          setShowModal(true);
+        }else{
+          setCompWins(compWins+1);
+          setCompFlag(0);
+          setPlayerFlag(0);
+          // alert('You Lose :(');
+          setShowModal(true);
+        }
+    }
+    
+   
+    }
+
+    // ini yang ori
+
+    // if (playerChoice === computerChoice) {
+    //   setResult('It\'s a tie!');
+    // } else if (
+    //   (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    //   (playerChoice === 'paper' && computerChoice === 'rock') ||
+    //   (playerChoice === 'scissors' && computerChoice === 'paper')
+    // ) {
+    //   setResult('You win!');
+    //   setPlayerFlag(playerFlag + 1);
+    // } else {
+    //   setResult('Computer wins!');
+    // }
+
+    // if (round < roundTotal) {
+    //   setRound(round + 1);
+    // }else{
+    //   setRound(1);
+    // }
+  
+  };
+
+  return (
+    <div>
+      <h1>Rock, Paper, Scissors</h1>
+      <h1>Yang sampe {allRound} duluan menang! </h1>
+      {/* <h2>Round {round}</h2> */}
+    
+      <div>
+        <button onClick={() => playRound('rock')}>Rock</button>
+        <button onClick={() => playRound('paper')}>Paper</button>
+        <button onClick={() => playRound('scissors')}>Scissors</button>
+      </div>
+      <div>
+        {playerChoice && (
+          <p>You chose: {playerChoice}</p>
+        )}
+        {computerChoice && (
+          <p>Computer chose: {computerChoice}</p>
+        )}
+        <p>{result}</p>
+        <p>Player Wins: {playerWins}</p>
+        <p>Computer Wins: {compWins}</p>
+
+      
+        <p>Player Flag: {playerFlag}</p>
+        <p>Computer Flag: {compFlag}</p>
+      
+      </div>
+    
+    </div>
+  );
+};
+
+
+
+export default PlayScreen;
+
+
+
